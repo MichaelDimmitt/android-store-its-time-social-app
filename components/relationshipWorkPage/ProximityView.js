475d7cae9a46eecb783bfdd8f39262f0275b99b0
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-12-31 14:49:31 -0500

    start building out a card

--- components/relationshipWorkPage/ProximityView.js
+++ components/relationshipWorkPage/ProximityView.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { black } from 'ansi-colors';
 
 export default class ProximityView extends Component {
   render() {
     return (
-      <View>
-        <Text style={styles.welcomeText}>Hello</Text>
+      <View style={styles.listContainer}>
+        <Text style={styles.welcomeText}>How often you are scheduling 1 on 1 </Text>
+        <Text style={styles.welcomeText}>How often are they scheduling 1 no 1.</Text>
+        
+        <Text style={styles.welcomeText}>How far you travel vs how far they travel.</Text>
         {/* <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Hello, world!</Text>
         </View> */}
@@ -14,6 +18,12 @@ export default class ProximityView extends Component {
   }
 }
 const styles = StyleSheet.create({
+    listContainer: {
+        borderWidth: 1,
+        borderColor: 'black',
+        margin: 20,
+        paddingTop: 20
+    },
     welcomeText: {
         marginLeft: 20,
         marginBottom: 20,
